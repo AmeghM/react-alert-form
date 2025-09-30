ad0d73430683656b7ff617265c3f47481a7ac130
AmeghM <ameghm407@gmail.com>
2025-09-30 22:25:06 +0530

    Add files

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   // base: "/ReactFormAlert",
-  base: import.meta.env.VITE_BASE_PATH || "/",
+  base: "/",
 });
